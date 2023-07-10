87b5578f99d934a6649e73c719c5774e7c98efab
Ahmet Taşdemir <ahmettsdmr1312@gmail.com>
2023-07-10 18:57:16 +0300

    gym exercise

---
+++ src/utils/fetchData.js
@@ -0,0 +1,16 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': API_KEY,
+    },
+};
+
+
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    return data;
+};
